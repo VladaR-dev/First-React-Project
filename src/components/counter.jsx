e3bb1aed9d6af0beb7833f44a56ed2e99c298651
Vlada Rakitina <vlada@gmail.com>
2024-10-11 23:39:11 +0300

    dinamic render classes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,9 +3,16 @@ import React from "react";
 const Counter = () => {
   const count = 0;
   const formatCount = () => (count === 0 ? "empty" : count);
+
+  const getBadgeClasses = () => {
+    let classes = "badge m-2 ";
+    classes += count === 0 ? "bg-warning" : "bg-primary";
+    return classes;
+  };
+
   return (
     <>
-      <span className="badge bg-primary m-2">{formatCount()}</span>
+      <span className={getBadgeClasses()}>{formatCount()}</span>
       <button className="btn btn-primary btn-sm m-2">+</button>
     </>
   );
