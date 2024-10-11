817d2d86606bd039918e718350e5449cd9fca206
Vlada Rakitina <vlada@gmail.com>
2024-10-11 20:17:29 +0300

    span and button styling

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,8 +5,8 @@ const Counter = () => {
   const formatCount = () => (count === 0 ? "empty" : count);
   return (
     <>
-      <span>{formatCount()}</span>
-      <button>+</button>
+      <span className="badge bg-primary m-2">{formatCount()}</span>
+      <button className="btn btn-primary btn-sm m-2">+</button>
     </>
   );
 };
