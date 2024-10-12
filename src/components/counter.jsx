f5fbf6cd0f2a64f97ecdee345709d110463e7bec
Vlada Rakitina <vlada@gmail.com>
2024-10-12 16:26:52 +0300

    conditional rendering

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -21,24 +21,30 @@ const Counter = () => {
   };
 
   const handleTagChange = (id) => {
-    // setTags(["tag4", "tag5"]);
     setTags((prevState) => prevState.filter((tag) => tag !== id));
     console.log(id);
   };
 
+  const renderTags = () => {
+    return (
+      tags.length !== 0 &&
+      tags.map((tag) => (
+        <li
+          key={tag}
+          className="btn btn-primary btn-sm m-2"
+          onClick={() => handleTagChange(tag)}
+        >
+          {tag}
+        </li>
+      ))
+    );
+  };
+
+  if (tags.length !== 0) {
+    return <ul>{renderTags()}</ul>;
+  }
   return (
     <>
-      <ul>
-        {tags.map((tag) => (
-          <li
-            key={tag}
-            className="btn btn-primary btn-sm m-2"
-            onClick={() => handleTagChange(tag)}
-          >
-            {tag}
-          </li>
-        ))}
-      </ul>
       <span className={getBadgeClasses()}>{formatCount()}</span>
       <button className="btn btn-primary btn-sm m-2" onClick={handleIncrement}>
         +
